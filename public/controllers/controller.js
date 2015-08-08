53e50f23e25955e080d33b5c7419273a29f6e0f0
zhenting zhou <zhentzhou@gmail.com>
2015-08-08 16:05:16 -0400

    basic func build

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,56 @@
 var myApp = angular.module('myApp',[]);
+
 myApp.controller('AppCtrl', ['$scope','$http',function($scope,$http){
 	console.log("Hello from controller");
+
+	//func gets data from server, send html with new data
+	var refresh = function(){	
+		//ask server for data capture the json data with response 
+		$http.get('/quotelist').success(function(response){
+			console.log("I got the data I requested");
+			$scope.quotelist = response;
+			//send obj with data to html
+			console.log(response);
+			$scope.quote = "";
+		})
+	};
+
+	//upon loading the page refresh page with new data from server
+	refresh();
+
+	//send data from html to server
+	$scope.addQuote = function (){
+		console.log('sending data from html.');
+		//console.log($scope.quote)
+		$http.post('/quotelist', $scope.quote).success(function(response){
+			console.log(response);
+			refresh();
+		})
+	};
+
+	//grabs the db id from html angular held data, send to delete route
+	$scope.remove = function (id){
+		console.log(id);
+		$http.delete('/quotelist/' + id).success(function(response){
+			refresh();
+		})
+	};
+
+	//grabs from db id input into html and insert into html 
+	$scope.edit = function (id){
+		//console.log(id);
+		$http.get('/quotelist/' + id).success(function(response){
+			$scope.quote = response;
+		})
+	};
+
+	//updates 
+	$scope.update = function (){
+		//logs the id
+		console.log($scope.quote._id);
+		$http.put('/quotelist/' + $scope.quote._id, $scope.quote).success(function(response){
+			refresh();
+		})
+	};
+
 }]);
--- server.js
+++ server.js
@@ -1,13 +1,77 @@
 var express = require('express');
 var app = express();
-
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+//mongo database and collection
+var db = mongojs('mydb', ['quote']);
 /*
 app.get('/', function(req, res){
 	res.send("Hello world from server.js")
 });
 */
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
+//get route listens for request for data
+app.get('/quotelist', function(req,res){
+	console.log("recevied a GET request");
+/*
+	//dummy data
+	quote1 = {
+		quote: 'Red apples are delish.',
+		author: 'Tim',
+		category: 'Food'
+	};
+	var contactlist = [quote1,quote2,quote3];
+	//respond by sending data vis json
+	res.json(contactlist)
+*/
+	db.quote.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	})
+});
+
+//post route listens for incomming data
+app.post('/quotelist', function(req,res){
+	console.log('POST route is reached in console');
+	console.log(req.body);
+
+	db.quote.insert(req.body, function(err, doc){
+		res.json(doc);
+	})
+});
+
+//delete route deletes from db
+app.delete('/quotelist/:id', function(req, res){
+	var id = req.params.id;
+	console.log(id);
+	//remove obj by id in mongo
+	db.quote.remove({_id:mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	})
+})
+
+app.get('/quotelist/:id', function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.quote.findOne({_id:mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+})
+
+app.put('/quotelist/:id', function(req,res){
+	var id = req.params.id;
+	console.log(req.body.quote);
+	db.quote.findAndModify(
+		{
+			query: {_id: mongojs.ObjectId(id)},
+			update: {$set: {quote: req.body.quote,author: req.body.author,category: req.body.category}}, 
+			new: true
+		}, function (err, doc) {
+			res.json(doc);
+		})
+});
 
 app.listen(3000);
 console.log("Server running on port 3000 captinn")
